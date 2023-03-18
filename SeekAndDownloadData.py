a2bbe6d2b407585aac18d5346c131545df89e342
VoidNinja900 <mike1doe@icloud.com>
2023-03-17 23:49:16 -0400

    pushed progress

--- SeekAndDownloadData.py
+++ SeekAndDownloadData.py
@@ -4,10 +4,15 @@ import time
 
 print("Sure, Why not?")
 
-path = r"%USERPROFILE%\Downloads"
+path = r"C:/Users/mike1/Downloads"
 
 
 for x in os.listdir(path):
-    if x.endswith('.json'):
-        os.path.join('DataOfMatch', x)
-        open(x, "x")
+    if x.endswith('.json') & x.__contains__('-'):
+        name = os.path.join('DataOfMatch/', x)
+        f = open(name, "x")
+        with open(path + "/" + x) as json_file:
+            data = json.load(json_file)
+            Converted = json.dumps(data)
+            f.write(Converted)
+            f.close
